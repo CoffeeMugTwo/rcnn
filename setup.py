f989ac96da25a6825514e9423bcfbfd8d95312da
Thomas Kittler <thomas.kittler.ext@siemens-healthineers.com>
2020-08-27 09:14:03 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A simple template for an R-CNN for object detection.',
+    author='Thomas Kittler',
+    license='',
+)
